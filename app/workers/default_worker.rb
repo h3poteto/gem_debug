6b7228fda6152d9245be40749ca1d85e4fe30b9e
AkiraFukushima <h3.poteto@gmail.com>
2017-08-23 11:54:51 +0900

    Use localstack instead of fake_sqs

--- app/workers/default_worker.rb
+++ app/workers/default_worker.rb
@@ -1,9 +1,9 @@
-class EventWorker
+class DefaultWorker
   include Shoryuken::Worker
 
   shoryuken_options queue: "event", auto_delete: true
 
   def perform(sqs_msg, message)
-    puts "shryuken event: #{message}"
+    puts "shoryuken default: #{message}"
   end
 end
--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -7,28 +7,45 @@ sqs_client = Aws::SQS::Client.new(
   raise_response_errors: false
 )
 queues = sqs_client.list_queues
-# queues.queue_urlsは
-# "http://0.0.0.0:4568/crowdworks-dev-copy-check-queue"
-# というstringが返ってくるので、比較のためにpathを抜き出す
-unless queues.queue_urls.map{|q| URI.parse(q).path }.any?{|p| /.*event/ =~ p }
-  new_queue = sqs_client.create_queue(queue_name: "event")
-  if new_queue
-    Rails.logger.info "#{new_queue.queue_url}を作成しました"
-  else
-    Rails.logger.error "#{sqs_client.config.endpoint}/eventの作成に失敗しました"
+if queues.successful?
+  YAML.load(ERB.new(IO.read(Rails.root.join("config/shoryuken.yml"))).result).
+    deep_symbolize_keys[:queues].each do |shoryuken_queue, _priority|
+    # queues.queue_urlsは
+    # "http://0.0.0.0:4568/crowdworks-development-copy-check-queue"
+    # というstringが返ってくるので、比較のためにpathを抜き出す
+    unless queues.queue_urls.map{|q| URI.parse(q).path }.any?{|p| /.*#{shoryuken_queue}/ =~ p }
+      new_queue = sqs_client.create_queue(queue_name: shoryuken_queue)
+      if new_queue
+        Rails.logger.info "#{new_queue.queue_url}を作成しました"
+      else
+        Rails.logger.error "#{sqs_client.config.endpoint}/#{shoryuken_queue}の作成に失敗しました"
+      end
+    end
   end
 end
 
-Shoryuken.sqs_client = Aws::SQS::Client.new(
-  endpoint: ENV["SQS_ENDPOINT"],
-  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-  region: ENV["AWS_REGION"],
-  verify_checksums: false
-)
-Shoryuken.sqs_client_receive_message_opts = {
-  attribute_names: %(
+Shoryuken.configure_client do |config|
+  config.sqs_client = Aws::SQS::Client.new(
+    endpoint: ENV["SQS_ENDPOINT"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    region: ENV["AWS_REGION"],
+    verify_checksums: false
+  )
+  config.sqs_client_receive_message_opts = {
+    attribute_names: %(
       ApproximateReceiveCount
       SentTimestamp
+    )
+  }
+end
+
+Shoryuken.configure_server do |config|
+  config.sqs_client = Aws::SQS::Client.new(
+    endpoint: ENV["SQS_ENDPOINT"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    region: ENV["AWS_REGION"],
+    verify_checksums: false
   )
-}
+end
